254120083a0df2f5e60003b39d592284854a7f63
Ismoiljon Jalolov <94523115+ismoil2000yl@users.noreply.github.com>
2023-10-11 12:52:23 +0300

    loader

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -4,13 +4,22 @@ import { Route, Routes } from "react-router-dom";
 import { authRoutes, privateRoutes } from "./index";
 import { get } from "lodash";
 import NotFound from 'pages/not found'
+import { Triangle } from "react-loader-spinner";
 
 const appRoutes = (routes) => {
   return routes.map((route, key) => (
     <React.Fragment key={key}>
       <Route
         path={route.path}
-        element={<Suspense fallback="">{route.element}</Suspense>}
+        element={<Suspense fallback={<div className="loading"><Triangle
+          height="100"
+          width="100"
+          color="#070F1D"
+          ariaLabel="triangle-loading"
+          wrapperStyle={{}}
+          wrapperClassName=""
+          visible={true}
+        /></div>}>{route.element}</Suspense>}
       />
       {route.children && appRoutes(route.children)}
     </React.Fragment>
